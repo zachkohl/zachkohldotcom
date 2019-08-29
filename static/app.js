43a4f7a2d7e544f228ef4ef52537b182b28deb45
zachkohl <zach.kohl@gmail.com>
2019-08-29 11:28:55 -0700

    hover works

--- static/app.js
+++ static/app.js
@@ -15,11 +15,25 @@ navlinks.forEach((link,index)=>{
         link.style.animation='navLinkFade 0.5s ease forwards' +time+'s';
     }
 })
+})
+};
 
+const entrance =()=>{
+    const aboutImage = document.getElementById('aboutImage');
+    const examplesImage = document.getElementById('exampleImage');
+    const contactImage = document.getElementById('databaseImage');
+ 
+aboutImage.addEventListener('mouseover',()=>{
+   aboutImage.classList.remove('')
 })
 
 
+}
 
-};
+const initialize=()=>{
+    navSlideAnimation();
+    entrance();
+}
+initialize();
 
-navSlideAnimation();
+
